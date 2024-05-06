b229e42529dd7542abc2fdee1cb5fef6788b1f9f
Harsh sahu <sahuharsh164@gmail.com>
2024-05-06 12:10:42 +0530

    profile pic by multer

--- app.js
+++ app.js
@@ -5,15 +5,33 @@ const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const postModel = require("./models/post.js");
-
+const crypto=require("crypto");
+const { log } = require("console");
+const path =require('path')
+ const upload=require("./config/multerConfig.js")
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname,"public")))
+
+ 
 
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.get("/profile/uploads", (req, res) => {
+  res.render("profileupload");
+});
+
+app.post("/upload",isLoggedIn,upload.single("image"), async(req, res)=>{
+let user=await userModel.findOne({email: req.user.email});
+user.profilepic=req.file.filename;
+await user.save()
+res.redirect("/profile")
+})
+ 
 
 app.get("/login", (req, res) => {
   res.render("login");
